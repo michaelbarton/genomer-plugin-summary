09c62268f8df32ac37b92650ffb814ddc59d1fdf
Michael Barton <mail@michaelbarton.me.uk>
2012-04-30 22:51:19 -0400

    Create gaps class

---
+++ lib/genomer-plugin-summary/gaps.rb
@@ -0,0 +1,4 @@
+require 'genomer'
+
+class GenomerPluginSummary::Gaps < Genomer::Plugin
+end
---
+++ spec/genomer-plugin-summary_spec/gaps_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+require 'genomer-plugin-summary/gaps'
+
+describe GenomerPluginSummary::Gaps do
+end
