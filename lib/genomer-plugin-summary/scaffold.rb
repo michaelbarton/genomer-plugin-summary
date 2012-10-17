61db7271e61a74927fd7c80eb3c84d6353f3419b
Michael Barton <mail@michaelbarton.me.uk>
2012-10-17 16:08:16 -0400

    Add tabulate method

--- lib/genomer-plugin-summary/scaffold.rb
+++ lib/genomer-plugin-summary/scaffold.rb
@@ -2,4 +2,42 @@ require 'genomer'
 require 'terminal-table'
 
 class GenomerPluginSummary::Scaffold < Genomer::Plugin
+
+  LAYOUT = [
+    {'Contigs (#)'  => :n_contigs},
+    {'Gaps (#)'     => :n_gaps},
+    :separator,
+    {'Size (bp)'    => :bp_size},
+    {'Contigs (bp)' => :bp_contigs},
+    {'Gaps (bp)'    => :bp_gaps},
+    :separator,
+    {'G+C (%)'      => :pc_gc},
+    {'Contigs (%)'  => :pc_contigs},
+    {'Gaps (%)'     => :pc_gaps}
+  ]
+
+  def title
+    'Scaffold'
+  end
+
+  def tabulate(data)
+    table = Terminal::Table.new(:title => title) do |t|
+      LAYOUT.each do |row|
+        t << if row == :separator
+               :separator
+             else
+               key   = row.keys.first
+               value = data[row.values.first]
+               value = sprintf('%#.2f',value) if value.class == Float
+               [key,value.to_s.rjust(9)]
+             end
+      end
+    end
+
+    table.align_column 0, :left
+    table.align_column 1, :right
+
+    table.to_s
+  end
+
 end
--- spec/genomer-plugin-summary_spec/scaffold_spec.rb
+++ spec/genomer-plugin-summary_spec/scaffold_spec.rb
@@ -2,4 +2,45 @@ require 'spec_helper'
 require 'genomer-plugin-summary/scaffold'
 
 describe GenomerPluginSummary::Scaffold do
+
+  describe "#tabulate" do
+
+    subject do
+      described_class.new([],{}).tabulate(data) + "\n"
+    end
+
+    context "passed table data" do
+
+      let(:data) do
+        {:n_contigs  =>   1,
+         :n_gaps     =>   0,
+         :bp_size    =>   4,
+         :bp_contigs =>   1,
+         :bp_gaps    =>   0,
+         :pc_gc      =>  50.0,
+         :pc_contigs => 100.0,
+         :pc_gaps    =>   0.0}
+      end
+
+      it do
+        should ==<<-EOS.unindent!
+      +--------------+-----------+
+      |         Scaffold         |
+      +--------------+-----------+
+      | Contigs (#)  |         1 |
+      | Gaps (#)     |         0 |
+      +--------------+-----------+
+      | Size (bp)    |         4 |
+      | Contigs (bp) |         1 |
+      | Gaps (bp)    |         0 |
+      +--------------+-----------+
+      | G+C (%)      |     50.00 |
+      | Contigs (%)  |    100.00 |
+      | Gaps (%)     |      0.00 |
+      +--------------+-----------+
+        EOS
+      end
+    end
+  end
+
 end
