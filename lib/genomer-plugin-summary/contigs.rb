11f765e9344635800c2f76b740210556b1a843c0
Michael Barton <mail@michaelbarton.me.uk>
2012-10-30 14:55:18 -0400

    Tabulate function for contig summary

--- lib/genomer-plugin-summary/contigs.rb
+++ lib/genomer-plugin-summary/contigs.rb
@@ -1,4 +1,42 @@
 require 'genomer'
+require 'genomer-plugin-summary/format'
 
 class GenomerPluginSummary::Contigs < Genomer::Plugin
+  include GenomerPluginSummary::Format
+
+  FORMATTING = {
+    :title   => 'Scaffold Contigs',
+    :headers => ['Contig', 'Start (bp)', 'End (bp)', 'Size (bp)', 'Size (%)', 'GC (%)'],
+    :width => {
+      0 => 6,
+      1 => 10,
+      2 => 10,
+      3 => 10,
+      4 => 8,
+      5 => 6
+    },
+    :justification => {
+      0 => :right,
+      1 => :right,
+      2 => :right,
+      3 => :right,
+      4 => :right,
+      5 => :right
+    },
+    :format => {
+      4 => '%#.2f',
+      5 => '%#.2f'
+    }
+  }
+  COLUMNS = [:num, :start, :stop, :size, :percent, :gc]
+
+  def tabulate(contigs,total,flags)
+    rows = contigs.map{|contig| COLUMNS.map{|col| contig[col]}}.
+      <<(:separator).
+      <<(COLUMNS.map{|col| total[col] || 'All'})
+
+    FORMATTING[:output] = flags[:output]
+    table(rows,FORMATTING)
+  end
+
 end
--- spec/genomer-plugin-summary_spec/contigs_spec.rb
+++ spec/genomer-plugin-summary_spec/contigs_spec.rb
@@ -2,4 +2,146 @@ require 'spec_helper'
 require 'genomer-plugin-summary/contigs'
 
 describe GenomerPluginSummary::Contigs do
+
+  def row(num,start,stop,percent,gc)
+    {:num      => num,
+     :start    => start,
+     :stop     => stop,
+     :size     => (stop - start) + 1,
+     :percent  => percent,
+     :gc       => gc}
+  end
+
+  describe "#tabulate" do
+
+    subject do
+      described_class.new([],{}).tabulate(contigs,total,flags)
+    end
+
+    let(:flags) do
+      {}
+    end
+
+    context "passed an empty array" do
+
+      let(:contigs) do
+        []
+      end
+
+      let(:total) do
+        {:start   => 0,
+         :stop    => 0,
+         :size    => 0,
+         :percent => 0,
+         :gc      => 0 }
+      end
+
+      it do
+        should ==<<-EOS.unindent!
+      +--------+------------+------------+------------+----------+--------+
+      |                         Scaffold Contigs                          |
+      +--------+------------+------------+------------+----------+--------+
+      | Contig | Start (bp) |  End (bp)  | Size (bp)  | Size (%) | GC (%) |
+      +--------+------------+------------+------------+----------+--------+
+      +--------+------------+------------+------------+----------+--------+
+      |    All |          0 |          0 |          0 |     0.00 |   0.00 |
+      +--------+------------+------------+------------+----------+--------+
+        EOS
+      end
+
+    end
+
+    context "passed an array with a single row" do
+
+      let(:contigs) do
+        [row(1,1,4,100.0,50.0)]
+      end
+
+      let(:total) do
+        {:start   => '1',
+         :stop    => '4',
+         :size    => '4',
+         :percent => 100.0,
+         :gc      => 50.0 }
+      end
+
+      it do
+        should ==<<-EOS.unindent!
+      +--------+------------+------------+------------+----------+--------+
+      |                         Scaffold Contigs                          |
+      +--------+------------+------------+------------+----------+--------+
+      | Contig | Start (bp) |  End (bp)  | Size (bp)  | Size (%) | GC (%) |
+      +--------+------------+------------+------------+----------+--------+
+      |      1 |          1 |          4 |          4 |   100.00 |  50.00 |
+      +--------+------------+------------+------------+----------+--------+
+      |    All |          1 |          4 |          4 |   100.00 |  50.00 |
+      +--------+------------+------------+------------+----------+--------+
+        EOS
+      end
+
+    end
+
+    context "passed a array with two rows" do
+
+      let(:contigs) do
+        [row(1,1,4,100.0,50.0),
+         row(2,1,4,100.0,50.0)]
+      end
+
+      let(:total) do
+        {:start   => '1',
+         :stop    => '4',
+         :size    => '4',
+         :percent => 100.0,
+         :gc      => 50.0 }
+      end
+
+      it do
+        should ==<<-EOS.unindent!
+      +--------+------------+------------+------------+----------+--------+
+      |                         Scaffold Contigs                          |
+      +--------+------------+------------+------------+----------+--------+
+      | Contig | Start (bp) |  End (bp)  | Size (bp)  | Size (%) | GC (%) |
+      +--------+------------+------------+------------+----------+--------+
+      |      1 |          1 |          4 |          4 |   100.00 |  50.00 |
+      |      2 |          1 |          4 |          4 |   100.00 |  50.00 |
+      +--------+------------+------------+------------+----------+--------+
+      |    All |          1 |          4 |          4 |   100.00 |  50.00 |
+      +--------+------------+------------+------------+----------+--------+
+        EOS
+      end
+
+    end
+
+    context "passed the csv output option" do
+
+      let(:flags) do
+        {:output => 'csv'}
+      end
+
+      let(:contigs) do
+        [row(1,1,4,100.0,50.0),
+         row(2,1,4,100.0,50.0)]
+      end
+
+      let(:total) do
+        {:start   => '1',
+         :stop    => '4',
+         :size    => '4',
+         :percent => 100.0,
+         :gc      => 50.0 }
+      end
+
+      it do
+        should ==<<-EOS.unindent!
+          contig,start_bp,end_bp,size_bp,size_%,gc_%
+          1,1,4,4,100.00,50.00
+          2,1,4,4,100.00,50.00
+          all,1,4,4,100.00,50.00
+        EOS
+      end
+
+    end
+
+  end
 end
