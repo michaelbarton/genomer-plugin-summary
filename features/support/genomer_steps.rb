146d751d4a51231b0c8d94ceca19dc67e834b81b
Michael Barton <mail@michaelbarton.me.uk>
2013-01-22 10:25:04 -0500

    Update man page for gaps summary

--- features/support/genomer_steps.rb
+++ features/support/genomer_steps.rb
@@ -3,3 +3,9 @@ When /^I create a new genomer project$/ do
   step 'I cd to "project"'
   step 'I append to "Gemfile" with "gem \'genomer-plugin-summary\', :path =>\'../../../\'"'
 end
+
+Then /^the output should contain a valid man page$/ do
+  step 'the output should not contain "md2man/roff: raw_html not implemented"'
+  step 'the output should not contain "\<"'
+  step 'the output should not contain "\>"'
+end
